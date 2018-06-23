b6571669030211dbae2bb61f6434fe049340d14a
danbuckland1 <buckd409@gmail.com>
2018-06-22 22:32:45 -0400

    Made initial commit and added html,css, and js. Also, created variable of comedians.

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var comedians = ['John Belushi', 'Steve Martin', 'Bill Murray', 'Dave Chappelle', 'Eddie Murphy', 'Jerry Seinfeld', 'George Carlin', 'Garry Chandling', 'Pete Holmes', 'John Mulaney', 'Chris Rock'];
+
+for(i = 0; i < 10; i++){
+console.log(comedians);
+};
+
+

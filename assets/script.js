df70fc8dcbdbcc625c879a871e189c4fb17d1b4c
danbuckland1 <buckd409@gmail.com>
2018-06-24 21:54:29 -0400

    Added buttons and working on submitting form data to create new buttons

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,52 @@
-var comedians = ['John Belushi', 'Steve Martin', 'Bill Murray', 'Dave Chappelle', 'Eddie Murphy', 'Jerry Seinfeld', 'George Carlin', 'Garry Chandling', 'Pete Holmes', 'John Mulaney', 'Chris Rock'];
 
-for(i = 0; i < 10; i++){
-console.log(comedians);
-};
+var sodas = ['Coke', 'Sprite', 'Mountain Dew', 'Dr. Pepper', 'Root Beer'];
 
+
+function displaySodaGifs(){
+    var soda = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=AfBXBVWRioadKrXZOqJFse9JxinigJyO&q=soda&limit=10&offset=0&rating=G&lang=en" + soda + "AfBXBVWRioadKrXZOqJFse9JxinigJyO";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+     }).then(function(response){
+         var sodaDiv = $("<div class='soda'>");
+         var rating = response.Rated;
+         var p = $("<p>").text("Rating: " + rating);
+         movieDiv.append(p);
+
+     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// function renderButtons(){
+
+// $('#soda-display').empty();
+// for(i = 0; i < soda.length; i++){
+//     var button = $('<button>').html(soda[i]);
+//     $('#soda-display').append(button);
+//   }
+// }
+
+// $('#add-soda').on('click', function(event){
+//     event.preventDefault();
+
+//     renderButtons();
+
+//     var sodaInput = $('#soda-input').val();
+//     soda.push(sodaInput);
+// });
+
+
+// renderButtons();
 
